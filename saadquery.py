c506c860791525110bedc97f26cbaeda857ad713
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-05-08 06:57:32 +1000

    Started to really make the saadquery.create function useful. Fleshed out some table structure and added dummy values.

--- saadquery.py
+++ saadquery.py
@@ -3,13 +3,37 @@ import sqlite3
 def create():
 	con = sqlite3.connect('saad.db')
 	c = con.cursor()
-	# Create Categories table
-	c.execute('''CREATE TABLE IF NOT EXISTS Categories(name TEXT PRIMARY KEY);''')
-	# Insert categories
-	c.execute('''INSERT INTO Categories (name) VALUES ('appliances');''')
-	c.execute('''INSERT INTO Categories (name) VALUES ('structural');''')
-	con.commit()
-	c.close()
+	
+	try:
+		# Create ApplianceCategories table.
+		c.execute('''DROP TABLE IF EXISTS ApplianceCategories;''')
+		c.execute('''CREATE TABLE ApplianceCategories(name TEXT PRIMARY KEY);''')
+		# Insert categories.
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Bathroom');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Washing');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Drying');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Kitchen');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Entertainment');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Heating and cooling');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Office');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Outdoor');''')
+		
+		# Create Appliances table.
+		c.execute('''DROP TABLE IF EXISTS Appliances;''')
+		c.execute('''CREATE TABLE IF NOT EXISTS Appliances(
+			name TEXT PRIMARY KEY NOT NULL,
+			category TEXT NOT NULL,
+			power REAL,
+			offPower REAL,
+			FOREIGN KEY(category) REFERENCES ApplianceCategories(name));''')
+		# Add some dummy appliances.
+		c.execute('''INSERT INTO Appliances VALUES ('Single tub top-loader', 'Washing', 477, 0.5);''')
+		c.execute('''INSERT INTO Appliances VALUES ('Bar radiator', 'Heating and cooling', 800, NULL);''')
+		c.execute('''INSERT INTO Appliances VALUES ('Webber Q', 'Outdoor', NULL, NULL);''')
+	
+	finally:
+		con.commit()
+		c.close()
 
 def get_categories():
 	con = sqlite3.connect('saad.db')
