152ccdba23a0f61ee4feeec0f262801198d29ec2
Buckmaster; Daniel Keith <Buckmaster; Daniel Keith dbuc6168@ug.it.usyd.edu.au>
2012-05-09 17:43:12 +1000

    Started to add proper functionality to javascript interface.

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+FEATURE = '';
+
+function selectFeature(var f) {
+	$('a.feature').removeClass('selected');
+	f.addClass('selected');
+	FEATURE = f.html();
+}
+
+function selectCategory(var c) {
+	// Clear appliances list.
+	$('#appliances').empty();
+	// Get list of appliances within category.
+	var url = 'http://localhost/appliances/' + c.html();
+    $.get(url, function(data) {
+		var appliances = $('#appliances');
+		for(line in data.split("\n")) {
+			category.append('<option>' + data[line] + '</option>');
+		}
+    });
+}
+
 $().ready(function() {
+	// Update feature when link clicked.
+	$('a.feature').click(function() {
+		selectFeature($(this));
+	});
+	// First feature selected by default.
+	selectFeature($('a.feature:first'));
+	
+	// Fill in categories dropdown.
 	var url = 'http://localhost/categories';
     $.get(url, function(data) {
-		alert(data);
+		var categories = $('#categories');
+		for(line in data.split("\n")) {
+			categories.append('<option>' + data[line] + '</option>');
+		}
     });
 	
-	// Select categories.
-	$('a.feature').click(function() {
-		$('a.feature').removeClass('selected');
-		$(this).addClass('selected');
-	});
-	// First category selected by default.
-	$('a.feature:first').addClass('selected');
+	// Update category when item selected.
+	
+	// Update page content when appliance clicked.
 });
