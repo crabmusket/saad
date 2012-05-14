179667e8ac232a5c3ed94d8e89df3c84cd2aeecb
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-05-14 19:49:35 +1000

    Modified server/database query interface.

--- saadquery.py
+++ saadquery.py
@@ -10,13 +10,21 @@ def create():
 		c.execute('''CREATE TABLE ApplianceCategories(name TEXT PRIMARY KEY);''')
 		# Insert categories.
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Bathroom');''')
+		c.execute('''INSERT INTO ApplianceCategories VALUES ('Heating and cooling');''')
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Washing');''')
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Drying');''')
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Kitchen');''')
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Entertainment');''')
-		c.execute('''INSERT INTO ApplianceCategories VALUES ('Heating and cooling');''')
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Office');''')
 		c.execute('''INSERT INTO ApplianceCategories VALUES ('Outdoor');''')
+		
+		# Create StructureCategories table.
+		c.execute('''DROP TABLE IF EXISTS StructureCategories;''')
+		c.execute('''CREATE TABLE StructureCategories(name TEXT PRIMARY KEY);''')
+		# Insert categories.
+		c.execute('''INSERT INTO StructureCategories VALUES ('Windows');''')
+		c.execute('''INSERT INTO StructureCategories VALUES ('Lighting');''')
+		c.execute('''INSERT INTO StructureCategories VALUES ('Floors');''')
 		
 		# Create Appliances table.
 		c.execute('''DROP TABLE IF EXISTS Appliances;''')
@@ -30,15 +38,22 @@ def create():
 		c.execute('''INSERT INTO Appliances VALUES ('Single tub top-loader', 'Washing', 477, 0.5);''')
 		c.execute('''INSERT INTO Appliances VALUES ('Bar radiator', 'Heating and cooling', 800, NULL);''')
 		c.execute('''INSERT INTO Appliances VALUES ('Webber Q', 'Outdoor', NULL, NULL);''')
+		c.execute('''INSERT INTO Appliances VALUES ('Hair dryer', 'Bathroom', NULL, NULL);''')
 	
 	finally:
 		con.commit()
 		c.close()
 
-def get_categories():
+def get_appliance_categories():
 	con = sqlite3.connect('saad.db')
 	c = con.cursor()
-	c.execute('''SELECT * FROM Categories;''')
+	c.execute('''SELECT * FROM ApplianceCategories;''')
+	return [cat[0] for cat in c.fetchall()]
+
+def get_structure_categories():
+	con = sqlite3.connect('saad.db')
+	c = con.cursor()
+	c.execute('''SELECT * FROM StructureCategories;''')
 	return [cat[0] for cat in c.fetchall()]
 
 def get_appliances(category = None):
@@ -48,4 +63,7 @@ def get_appliances(category = None):
 		c.execute('''SELECT * FROM Appliances WHERE category = '{0}';'''.format(category))
 	else:
 		c.execute('''SELECT * FROM Appliances;''')
-	return c.fetchall()
+	return [app[0] for app in c.fetchall()]
+
+def get_structures(category = None):
+	return ""
--- server.py
+++ server.py
@@ -7,22 +7,37 @@ def create_db(options):
 
 def get_categories(options):
 	cats = ""
-	for c in saadquery.get_categories():
-		cats += c + "\n"
+	if len(options) == 0:
+		raise Exception()
+	if options[0] == "appliances":
+		categories = saadquery.get_appliance_categories()
+	else:
+		categories = saadquery.get_structure_categories()
+	for c in categories:
+		cats += str(c) + "\n"
 	return cats
 
 def get_appliances(options):
-	if len(options) == 0
-		options = None
+	if len(options) == 0:
+		options = [None]
 	apps = ""
-	for a in saadquery.get_appliances(options):
-		apps += a + "\n"
+	for a in saadquery.get_appliances(options[0]):
+		apps += str(a) + "\n"
 	return apps
+
+def get_structures(options):
+	if len(options) == 0:
+		options = [None]
+	strucs = ""
+	for s in saadquery.get_structures(options[0]):
+		strucs += str(s) + "\n"
+	return strucs
 
 GETHANDLERS = {
 	"create": create_db,
 	"categories": get_categories,
 	"appliances": get_appliances,
+	"structures": get_structures,
 }
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -35,9 +50,14 @@ class MyHandler(BaseHTTPRequestHandler):
 			try:
 				words = self.path[1:].split('/')
 				fn = GETHANDLERS[words[0]]
-				self.wfile.write(fn(words[1:]).encode("utf-8"))
+				options = []
+				if len(words) > 1:
+					options = words[1:]
+				self.wfile.write(fn(options).encode("utf-8"))
 			except KeyError:
 				self.wfile.write(self.path.encode("utf-8"))
+			except Exception as e:
+				self.wfile.write(str(e).encode("utf-8"))
 			return
 		except:
 			self.send_error(404, 'File Not Found: {0}'.format(self.path))
