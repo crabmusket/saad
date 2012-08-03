1b3ee8a433db77be8e83bf6594b60ddb29b56a69
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-08-03 15:52:32 +1000

    Updated helpers to a nicer format.

--- helpers.js
+++ helpers.js
@@ -1,32 +1,58 @@
-﻿function fmt_price(price) {
-	if(!price)
+﻿// Format a raw value with units. If pre evaluates to true, units are appended before value.
+function fmt_raw(value, units, pre) {
+	if(!value)
 		return "N/A";
-	else if(price.from && price.to)
-		return "$" + price.from + " - $" + price.to;
-	else {
-		str = Array();
-		for(type in price) {
-			str.push(type + ": " + fmt_price(price[type]));
+	if(pre)
+		return units + value;
+	return value + units;
+}
+
+// Format a range. If pre evaluates to true, units are appended before values.
+function fmt_from_to(value, units, pre) {
+	if(!value)
+		return "N/A";
+	if(pre)
+		return units + value.from + " - " + units + value.to;
+	return value.from + units + " - " + value.to + units;
+}
+
+// Returns a function that determines what kind of format to use.
+function fmt_flexible(units, pre) {
+	return function(value) {
+		if(!value)
+			return "N/A";
+		else if(value.from && value.to)
+			return fmt_from_to(value, units, pre);
+		else {
+			str = Array();
+			for(type in value) {
+				var entry = "N/A";
+				if(value[type].from && value[type].to)
+					entry = fmt_from_to(value[type], units, pre);
+				else {
+					console.log(value, type);
+					entry = fmt_raw(value[type], units, pre);
+				}
+				str.push(type + ": " + entry);
+			}
+			if(str.length == 0)
+				return fmt_raw(value, units, pre);
+			return str.join(", ");
 		}
-		return str.join(", ");
-	}
+	};
 }
 
-function fmt_power(power) {
-	if(!power)
-		return "N/A";
-	else if(power.from && power.to)
-		return power.from + " - " + power.to + "W";
-	else
-		return "something else";
-}
+Handlebars.registerHelper('dollars', fmt_flexible("$", true));
+Handlebars.registerHelper('watts',   fmt_flexible("W"));
+Handlebars.registerHelper('wpl',     fmt_flexible("W/L"));
+Handlebars.registerHelper('hours',   fmt_flexible(" hours"));
 
-// Handlebars helpers
-Handlebars.registerHelper('id', function(str) {
+// Converts a messy string to an ID.
+function cleanString(str) {
 	if(str)
 		return "app-" + str.replace(/\s/g, "-").replace(/[\.,?\/#!$%\^&\*;:{}=\_`~()]/g,"").toLowerCase();
 	else
 		return "";
-});
-Handlebars.registerHelper('fmt_price', fmt_price);
-Handlebars.registerHelper('fmt_power', fmt_power);
+}
+
+Handlebars.registerHelper('id', cleanString);
--- saad.js
+++ saad.js
@@ -1,36 +1,3 @@
-function fmt_price(price) {
-	if(!price)
-		return "N/A";
-	else if(price.from && price.to)
-		return "$" + price.from + " - $" + price.to;
-	else {
-		str = Array();
-		for(type in price) {
-			str.push(type + ": " + fmt_price(price[type]));
-		}
-		return str.join(", ");
-	}
-}
-
-function fmt_power(power) {
-	if(!power)
-		return "N/A";
-	else if(power.from && power.to)
-		return power.from + " - " + power.to + "W";
-	else
-		return "something else";
-}
-
-// Handlebars helpers
-Handlebars.registerHelper('id', function(str) {
-	if(str)
-		return "content-" + str.replace(/\s/g, "-").replace(/[\.,?\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
-	else
-		return "";
-});
-Handlebars.registerHelper('fmt_price', fmt_price);
-Handlebars.registerHelper('fmt_power', fmt_power);
-
 // Clears the currently-viewed item.
 function clearBrowser() {
 	var browser = $("#item-panel");
