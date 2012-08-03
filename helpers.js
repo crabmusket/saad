7262f5ad7a9abc11ce7c8fce1c3b6c80aed32d07
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-08-03 15:21:18 +1000

    Moved Handlebars helpers into their own file.

---
+++ helpers.js
@@ -0,0 +1,32 @@
+﻿function fmt_price(price) {
+	if(!price)
+		return "N/A";
+	else if(price.from && price.to)
+		return "$" + price.from + " - $" + price.to;
+	else {
+		str = Array();
+		for(type in price) {
+			str.push(type + ": " + fmt_price(price[type]));
+		}
+		return str.join(", ");
+	}
+}
+
+function fmt_power(power) {
+	if(!power)
+		return "N/A";
+	else if(power.from && power.to)
+		return power.from + " - " + power.to + "W";
+	else
+		return "something else";
+}
+
+// Handlebars helpers
+Handlebars.registerHelper('id', function(str) {
+	if(str)
+		return "app-" + str.replace(/\s/g, "-").replace(/[\.,?\/#!$%\^&\*;:{}=\_`~()]/g,"").toLowerCase();
+	else
+		return "";
+});
+Handlebars.registerHelper('fmt_price', fmt_price);
+Handlebars.registerHelper('fmt_power', fmt_power);
