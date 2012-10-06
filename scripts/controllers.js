dfd2575929fa93e1d5e2277d3a6195222b71a38f
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-10-06 15:16:41 +1000

    SaadBrowser now requests Google Doc data.
    The controller uses $http to request JSON from the spreadsheet API,
    then parses the nasty string that's returned into an object literal.
    Still needs more sanitising unfortunately.

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -4,9 +4,17 @@ function SaadQuery($scope) {
 }
 SaadQuery.$inject = ['$scope'];
 
-function SaadBrowser($scope) {
+function SaadBrowser($scope, $http) {
+  $http
+    .get('https://spreadsheets.google.com/feeds/list/0AruEbfhtGsHgdFZHNVI1dmxvajlxYkN5SmdsTWZyenc/od6/public/values?alt=json-in-script')
+    .success(function(data) {
+        var i = data.indexOf('{');
+        var json = data.substr(i, data.length - i - 2);
+        $scope.data = JSON.parse(json).feed.entry;
+        console.log($scope.data);
+      });
 }
-SaadBrowser.$inject = ['$scope'];
+SaadBrowser.$inject = ['$scope', '$http'];
 
 function SaadDetailView($scope, $routeParams) {
 }
