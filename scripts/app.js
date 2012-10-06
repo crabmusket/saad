f6a48fc54c303e1c63cdc4f86b29485d6bb6eaea
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-10-06 15:18:04 +1000

    Fixed a typo that killed the /broese route.

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // App module
-angular.module('saad', ['saadFilters', 'saadServices'])
+angular.module('saad', [])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/query', { templateUrl: 'partials/query.html', controller: SaadQuery })
-      .when('/browse', { templateURL: 'partials/browse.html', controller: SaadBrowser })
+      .when('/browse', { templateUrl: 'partials/browse.html', controller: SaadBrowser })
       .when('/view/:item', { templateUrl: 'partials/item-detail.html', controller: SaadDetailView })
       .otherwise({redirectTo: '/query'});
   }]);
